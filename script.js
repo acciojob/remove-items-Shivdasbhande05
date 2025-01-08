d5a1725fbcf420689a7559f184d15abfbf494f6a
Shivdasbhande05 <shivdasbhande@gmail.com>
2025-01-08 18:06:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+var selectEelement = document.getElementById("colorSelect");
+var removeButton = document.getElementById("removeButton");
+
+// we will add event listner on remove button
+removeButton.addEventListner("click", function() {
+	var selectedColor = selectEelement.selectedColor; // it will select color
+
+	// now we remove selected color;
+	selectEelement.remove(selectedColor);
+
+});
